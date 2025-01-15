dae401652f6b84bc3861956ad27a9b9a0cacad2b
Sami Koponen <samikoponen4@gmail.com>
2025-01-15 21:46:08 +0200

    Pieni korjaus.

--- Viikko1/T1/src/Main.java
+++ Viikko1/T1/src/Main.java
@@ -4,21 +4,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner lukija = new Scanner(System.in);
+
         int satunnainenLuku = (int) (Math.random() * 100 + 1);
+        System.out.print(satunnainenLuku);
         System.out.println("Tervetuloa peliin! Arvaa luku väliltä 1-100.");
+        int arvauksetMaksimi = 7;
         int arvaukset = 0;
-        System.out.println("Arvauksia jäljellä: " + (arvaukset + 7));
+
+        System.out.println("Arvauksia jäljellä: " + (arvaukset + arvauksetMaksimi));
 
         do {
+            System.out.print("Arvaa luku: ");
             int arvaus = lukija.nextInt();
 
             if(arvaus < satunnainenLuku){
                 System.out.println("Arvaamasi luku on pienempi kuin oikea luku.");
-                System.out.println("Arvauksia jäljellä: " + (6 - arvaukset));
+                System.out.println("Arvauksia jäljellä: " + (arvauksetMaksimi - arvaukset - 1));
             }
             else if(arvaus > satunnainenLuku) {
                 System.out.println("Arvaamasi luku on suurempi kuin oikea luku.");
-                System.out.println("Arvauksia jäljellä: " + (6 - arvaukset));
+                System.out.println("Arvauksia jäljellä: " + (arvauksetMaksimi - arvaukset - 1));
+
             }
             else {
                 System.out.println("Onneksi olkoon! Arvasit oikein!");
@@ -26,9 +32,11 @@ public class Main {
             }
             arvaukset++;
 
-        }while (arvaukset < 7);
+        }while (arvaukset < arvauksetMaksimi);
 
-        System.out.println("Hävisit pelin, oikea luku oli " + satunnainenLuku);
+        if(arvaukset == arvauksetMaksimi){
+            System.out.println("Hävisit pelin, oikea luku oli " + satunnainenLuku);
 
+        }
     }
 }
