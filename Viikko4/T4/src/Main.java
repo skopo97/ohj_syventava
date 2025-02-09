3944e20b77cf652d12ddea14480179abc17b4263
Sami Koponen <samikoponen4@gmail.com>
2025-02-09 21:07:40 +0200

    .

--- Viikko4/T4/src/Main.java
+++ Viikko4/T4/src/Main.java
@@ -3,33 +3,10 @@ import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+
+
 public class Main {
     public static void main(String[] args) {
-
-        String inputDate = null;
-        String inputCategory = null;
-
-
-        //Tarkistetaan, että syötettyjä parametreja on 2
-        if(args.length < 2 || args.length > 2){
-            System.out.println("Provide 2 arguments: date in format --MM-dd and " +
-                    "event category");
-            System.exit(1);
-        }else{
-            inputDate = args[0];
-            inputCategory = args[1];
-        }
-
-        //Tarkistetaan, että päivämäärä on oikeassa muodossa
-        MonthDay mDay = null;
-        try{
-            mDay = MonthDay.parse(inputDate, DateTimeFormatter.ofPattern("--MM-dd"));
-
-        }catch(DateTimeParseException e){
-            System.out.println("Invalid date, use format --MM-dd");
-            System.exit(1);
-        }
-
 
         //Luodaan Category-olio
         Category category = new Category("apple", "macOS");
@@ -43,31 +20,66 @@ public class Main {
                 new Event(LocalDate.of(2020, 11, 12), "Apple announces new Macbooks with Apple Silicon", category),
         };
 
+        String inputDate = null;
+        String inputCategory = null;
 
+
+        //Tarkistetaan, että syötettyjä parametreja on 2
+        if(args.length != 2){
+            System.out.println("Provide 2 arguments: date in format --MM-dd and " +
+                    "event category");
+            System.exit(1);
+        }else{
+            inputDate = args[0];
+            inputCategory = args[1].toLowerCase();
+
+            //Tarkistetaan, että syötetty kategoria on joko primary tai primary/secondary
+            if(inputCategory.equals(category.getSecondary().toLowerCase())){
+                System.out.println("Please provide primary category or primary/secondary category");
+                System.out.println("Valid categories found : " + category.getPrimary() + " and " + category.toString());
+                System.exit(1);
+            }
+        }
+        //Tarkistetaan, että päivämäärä on oikeassa muodossa
+        MonthDay mDay = null;
+        try{
+            mDay = MonthDay.parse(inputDate, DateTimeFormatter.ofPattern("--MM-dd"));
+
+        }catch(DateTimeParseException e){
+            System.out.println("Invalid date, use format --MM-dd");
+            System.exit(1);
+        }
+        //Tarkastetaan löytyykö syötettyä kategoriaa tapahtumista
+        boolean foundCategory = false;
+        for(Event e : events){
+            if(e.getCategory().getPrimary().toLowerCase().equals(inputCategory) ||
+                    e.getCategory().getSecondary().toLowerCase().equals(inputCategory) ||
+                    e.getCategory().toString().toLowerCase().equals(inputCategory)){
+                foundCategory = true;
+                break;
+            }
+        }
+        if (!foundCategory){
+            System.out.println("No events found for category : " + inputCategory);
+
+            System.exit(1);
+        }
         //Tulostetaan tapahtumat, jotka ovat samana päivänä kuin syötetty päivämäärä
         //Tarkastetaan myös kategoria
         //Boolean arvolla voidaan tulostaa myös, jos tapahtumia ei löydy
         boolean foundEvent = false;
-        int foundEvents = 0;
-                
         for(Event e : events){
             if(MonthDay.from(e.getDate()).equals(mDay) &&
-                    (e.getCategory().getPrimary().equals(inputCategory) ||
-                    e.getCategory().toString().equals(inputCategory))){
-                    foundEvents++;
+                    (e.getCategory().getPrimary().toLowerCase().equals(inputCategory) ||
+                    e.getCategory().toString().toLowerCase().equals(inputCategory))){
             }
         }
-        
         for(Event e : events){
             if(MonthDay.from(e.getDate()).equals(mDay) &&
-                    (e.getCategory().getPrimary().equals(inputCategory) ||
-                    e.getCategory().toString().equals(inputCategory))){
+                    (e.getCategory().getPrimary().toLowerCase().equals(inputCategory) ||
+                    e.getCategory().toString().toLowerCase().equals(inputCategory))) {
 
-        if(foundEvents > 1){
-            System.out.println(e.getDate().getYear() + " : " + e.getDescription());
-        }else{
-            System.out.println(e.getDescription());
-        }
+                System.out.println(e.getDate().getYear() + " : " + e.getDescription());
                 foundEvent = true;
             }
         }
