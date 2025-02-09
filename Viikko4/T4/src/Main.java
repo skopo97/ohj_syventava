767b1a44e4934b91bf5b756cc233721a8fb72e97
Sami Koponen <samikoponen4@gmail.com>
2025-02-09 19:57:20 +0200

    Haku päivämäärällä ja kategorialla toimii.

--- Viikko4/T4/src/Main.java
+++ Viikko4/T4/src/Main.java
@@ -7,15 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         String inputDate = null;
-        String description = null;
+        String inputCategory = null;
 
+
+        //Tarkistetaan, että syötettyjä parametreja on 2
         if(args.length < 2 || args.length > 2){
             System.out.println("Provide 2 arguments: date in format --MM-dd and " +
-                    "event description");
+                    "event category");
             System.exit(1);
         }else{
             inputDate = args[0];
-            description = args[1];
+            inputCategory = args[1];
         }
 
         //Tarkistetaan, että päivämäärä on oikeassa muodossa
@@ -25,6 +27,7 @@ public class Main {
 
         }catch(DateTimeParseException e){
             System.out.println("Invalid date, use format --MM-dd");
+            System.exit(1);
         }
 
 
@@ -37,20 +40,39 @@ public class Main {
                 new Event(LocalDate.of(2022,10,24), "macOS 13 Ventura released", category),
                 new Event(LocalDate.of(2021,10,25), "macOS 12 Monterey released", category),
                 new Event(LocalDate.of(2020,11,12), "macOS 11 Big Sur released", category),
+                new Event(LocalDate.of(2020, 11, 12), "Apple announces new Macbooks with Apple Silicon", category),
         };
 
+
         //Tulostetaan tapahtumat, jotka ovat samana päivänä kuin syötetty päivämäärä
+        //Tarkastetaan myös kategoria
+        //Boolean arvolla voidaan tulostaa myös, jos tapahtumia ei löydy
         boolean foundEvent = false;
+        int foundEvents = 0;
+                
         for(Event e : events){
-            if(MonthDay.from(e.getDate()).equals(mDay)){
-                System.out.println(e.getDescription());
+            if(MonthDay.from(e.getDate()).equals(mDay) &&
+                    (e.getCategory().getPrimary().equals(inputCategory) ||
+                    e.getCategory().toString().equals(inputCategory))){
+                    foundEvents++;
+            }
+        }
+        
+        for(Event e : events){
+            if(MonthDay.from(e.getDate()).equals(mDay) &&
+                    (e.getCategory().getPrimary().equals(inputCategory) ||
+                    e.getCategory().toString().equals(inputCategory))){
+
+        if(foundEvents > 1){
+            System.out.println(e.getDate().getYear() + " : " + e.getDescription());
+        }else{
+            System.out.println(e.getDescription());
+        }
                 foundEvent = true;
             }
         }
-
         if(!foundEvent){
             System.out.println("No events found on this date");
         }
-
     }
 }
